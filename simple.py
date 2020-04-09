45d23cbe965c6ad0a8c2fa2b81228a6814fe15af
kh3dron <tristansaldanha@gmail.com>
2020-04-08 21:42:48 -0700

    MORE THINGS WORK RIGHT
    runtime fixed & fast af now, mutations work, speed inheritance works right. Sense and ambition traits still need work

--- simple.py
+++ simple.py
@@ -17,12 +17,18 @@ def mate(a, b):
     name = (a.name) #sorry parent 2
     return(Biot(name, av(a.speed, b.speed), av(a.amb, b.amb), av(a.sense, b.sense)).mutateBiot())
 
+def legal_move(biot, news):
+    """Potential step is still inside the environment"""
+    dx = biot.coords[0] + news[0]
+    dy = biot.coords[1] + news[1]
+    return ((0 <= dx <= 100) and (0 <= dy <= 100))
+
 class Biot:
 
     def __init__(self, name, speed, amb, sense):
         """Spawn in with a handful of genes"""
         self.name = name #for debugging
-        self.speed = speed*5 #Distance covered in 1 step
+        self.speed = speed #Distance covered in 1 step
         self.amb = amb #Ambition is the probability that biot will go for reproduction
         self.sense = sense #how many steps away can food be seen from
         """Non-genetic attributes"""
@@ -58,15 +64,12 @@ class Biot:
                 self.eaten += 1
                 return r
 
-        #No food discovered, random step - but INSIDE the play area
-        rads = random.random() * 2 * math.pi
-        x, y = self.coords
+        #Map is a square, so one of 4 directions will work: choose randomly
+        steps = [(3, 3), (0, self.speed), (0, -1*self.speed), (self.speed, 0), (-1*self.speed, 0)]
+        legals = [g for g in steps if legal_move(self, g)]
+        move = legals[random.randint(0, len(legals)-1)]
 
-        dx, dy = (math.cos(rads)*self.speed, math.sin(rads)*self.speed)
-        while not (0 <= x+dx <= 100 and 0 <= y+dy <= 100):
-            rads = random.random() * 2 * math.pi
-            dx, dy = (math.cos(rads)*self.speed, math.sin(rads)*self.speed)
-        self.move((dx, dy))
+        self.move(move)
         return None
 
     def step_retreating(self):
@@ -86,19 +89,19 @@ class Biot:
             target = (x, 0)
 
         #If we're within one step of the edge
-        if distance(self.coords, target) < 5*self.speed:
+        if distance(self.coords, target) < self.speed:
             self.move(target)
             self.done = True
             return
         else:
             if target == (x, 100):
-                self.move(0, 5*self.speed)
+                self.move((0, self.speed))
             if target == (0, y):
-                self.move(((-5*self.speed), 0))
+                self.move(((-self.speed), 0))
             if target == (x, 0):
-                self.move((0, (-5*self.speed)))
+                self.move((0, (-self.speed)))
             if target == (100, y):
-                self.move(((5*self.speed), 0))
+                self.move(((self.speed), 0))
             return
 
     def roam(self, foods):
@@ -167,7 +170,7 @@ class Biot:
         return self.eaten >= 2*self.mtb
 
     def mutateTrait(self, trait):
-        return max(0, np.random.normal(trait, trait/15, 1)) #normal dist chance of moving by SD 1/10th from current spot
+        return np.random.normal(trait, trait/10.0, 1)[0]
 
     def mutateBiot(self):
         """All genes have a chance of changing by a small amount"""
@@ -241,6 +244,7 @@ class Field:
         """Run through a day, making some steps, then killing the weak and
         mutating the survivors."""
         self.refresh_all()
+        self.first_place_population()
         self.create_food()
         self.t_pop.append(len(self.population))
         for r in range(0, 20):
@@ -253,8 +257,6 @@ class Field:
         for z in self.population:
             if z.does_survive():
                 survivors.append(z)
-            else:
-                print("Biot died, food %lf/%lf" % (z.eaten, z.mtb))
 
         offspring = []
 
@@ -299,17 +301,17 @@ class Field:
             return("Biots Extinct")
 
 
-species = [Biot(str(t), 1, .8, 3) for t in range(0, 20)] #start with a simple group
+species = [Biot(str(t), 5, .8, 3) for t in range(0, 20)] #start with a simple group
 environment = Field(species)
-environment.simulate(100)
+environment.simulate(400)
 
 f, axarr = plt.subplots(2, 2)
 axarr[0, 0].plot(environment.t_speed)
-axarr[0, 0].set_title('Speed')
+axarr[0, 0].set_title('Evolved Speed')
 axarr[0, 1].plot(environment.t_sense)
-axarr[0, 1].set_title('Sense')
+axarr[0, 1].set_title('Evolved Sense')
 axarr[1, 0].plot(environment.t_mtb)
-axarr[1, 0].set_title('Metabolism')
+axarr[1, 0].set_title('Evolved Metabolism')
 axarr[1, 1].plot(environment.t_pop)
 axarr[1, 1].set_title('Population')
 
